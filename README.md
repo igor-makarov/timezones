f8df7309268e9c16b570706d68f094e57d54ed1e
Igor Makarov <igormaka@gmail.com>
2019-07-19 18:41:17 +0300

    rewrote to just transform Geonames data 😂 + JSON

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
 # TimeZones - lots of cities and their timezones
 
-This small project generates a CSV of many cities and their IANA timezones.  
+This small project generates a CSV (and JSON!) of many cities and their IANA timezones.  
 It was inspired by Dato by @sindresorhus which didn't have San Francisco!
 
-The output is deployed to Netlfy. It downloads a CC-BY list of about 13k cities and their coordinates from [simplemaps.com](https://simplemaps.com/data/world-cities).  
-Next, it runs it through [timezone_finder](https://github.com/gunyarakun/timezone_finder) to get the IANA timezone. 
+The output is deployed to Netlfy. It downloads a CC-BY list of about 25k cities and their coordinates from [Geonames](http://www.geonames.org/) and writes it into simplifed CSV and JSON files.  
 
-The output can be downloaded at (https://timezones.netlify.com/cities_time_zones.csv)
+The output can be downloaded at (https://timezones.netlify.com/cities_time_zones.csv) or (https://timezones.netlify.com/cities_time_zones.json).
--- generate.rb
+++ generate.rb
@@ -4,24 +4,45 @@
 require 'timezone_finder'
 require 'fileutils'
 require 'csv'
+require 'json'
 
 FileUtils.rm_rf('_site')
 FileUtils.mkdir_p('_site')
 
 Dir.chdir('_site') do
-  system('curl -o cities.zip https://simplemaps.com/static/data/world-cities/basic/simplemaps_worldcities_basicv1.5.zip')
+  system('curl -o cities.zip https://download.geonames.org/export/dump/cities15000.zip')
   system('unzip cities.zip')
 end
 
 tf = TimezoneFinder.create
 
-csv = CSV.read('_site/worldcities.csv')
+csv = CSV.open("_site/cities15000.txt", "r", { :col_sep => "\t" })
+
+cities = csv.map do |city|
+  _, name, _, keywords, lat, lon, _, _, country = city
+  time_zone = city[-2]
+  
+  # no need to lookup, Geonames data is super!
+  # just for validation
+  time_zone_calc = tf.timezone_at(lng: lon.to_f, lat: lat.to_f)
+  STDERR.puts "#{name} - #{time_zone_calc} != #{time_zone} (#{city[-1]})" if time_zone != time_zone_calc
+  
+  {
+    name: name,
+    country: country,
+    keywords: keywords,
+    time_zone: time_zone,
+  }
+end
+
+File.open('_site/cities_time_zones.json','w') do |output|
+  output.write JSON.pretty_generate(cities)
+end
+
 
 CSV.open('_site/cities_time_zones.csv', 'w') do |output|
-  csv.drop(1).each do |city|
-    name, _, lat, lon, country = city
-    time_zone = tf.timezone_at(lng: lon.to_f, lat: lat.to_f)
-    output << [name, country, time_zone]
+  cities.each do |city|
+    output << city.values
   end
 end
 
